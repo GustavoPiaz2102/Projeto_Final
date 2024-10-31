26de04e618d9f2f941d2fc18d8d1fa6f7c411b89
brunabhtd <brunabhtd@gmail.com>
2024-10-31 17:17:15 -0300

    fazendo a view enorme meu deus n terminei ainda

--- View/view.h
+++ View/view.h
@@ -10,5 +10,115 @@
 using namespace std;
 
 class View {
-    
+    public: 
+    static void bemvinde() {
+        cout << "Bem-vindo(a) a formação do line-up do Festival!";
+    }
+    static void incorreto() {
+        cout << "A opcao inserida e invalida.\n" << endl;
+    }
+    static void val_invalido(){
+        cout << "O valor inserido e invalido.\n" << endl;
+    }
+    static void sair(){
+        cout << "Fim do cadastro. Aproveite o show e até mais!\n" << endl;
+    }
+
+    static int menu(){
+        int opcao;
+        string msg = "";
+
+        mensagem += "Digite a opcao que voce deseja: \n";
+        mensagem += "1 - Adicionar musicas.\n";
+        mensagem += "2 - Adicionar integrantes.\n";
+        mensagem += "3 - Adicionar banda.\n";
+        mensagem += "4 - Adicionar show!\n";
+        mensagem += "5 - Visualizar musicas que irao tocar.\n";
+        mensagem += "6 - Visualizar os integrantes.\n";
+        mensagem += "7 - Visualizar as bandas.\n";
+        mensagem += "8 - Visualizar os shows que acontecerao.\n";
+        mensagem += "0 - Sair.\n";
+        cout << msg << endl;
+        cin >> opcao;
+        return opcao;
+    }
+
+    static void msg_musicas(int n_msg){
+        string msg = "";
+
+        switch (n_msg){
+            case 1:
+            msg = "Tudo certo. A musica foi inserida na lista com sucesso!\n";
+            case 2:
+            msg = "ERRO! Essa musica ja esta na lista.\n";
+        }
+        cout << msg << endl;
+    }
+
+    static void msg_integrantes(int n_msg){
+        string msg = "";
+        switch (n_msg){
+            case 1:
+            msg = "Tudo certo. O integrante foi inserido na lista com sucesso!\n";
+            case 2:
+            msg = "ERRO! Esse integrante ja esta na lista.\n";
+        }
+        cout << msg << endl;
+    }
+
+    static void msg_banda(int n_msg){
+        string msg = "";
+        switch (n_msg){
+            case 1:
+            msg = "Tudo certo. Essa banda foi inserida na lista com sucesso!\n";
+            case 2:
+            msg = "ERRO! Essa banda ja esta na lista.\n";
+        }
+        cout << msg << endl;
+    }
+
+    static void msg_show(int n_msg){
+        string msg = "";
+        switch (n_msg){
+            case 1:
+            msg = "Tudo certo. Esse show foi inserido na lista com sucesso!\n";
+            case 2:
+            msg = "ERRO! Esse show ja esta na lista.\n";
+        }
+        cout << msg << endl;
+    }
+
+    string void mostrar_musicas(const vector<musicas*>& Musica){
+        string msg = "";
+        int x=0;
+
+        msg += "Aqui estao os dados sobre as musicas!\n";
+
+        if (Musicas.empty()) {
+            mensagem += "Nenhuma musica foi adicionada ainda :(.";
+            } else {
+                for (const auto& Musica : Musicas){
+                    x++;
+                    msg = "Musica: " + to_string(x) + " Compositor: " + to_string(Musica->getMusica()) + "\n.";
+                }
+                msg += ""
+        }
+    }
+
+    string void mostrar_integrantes(const vector<integrantes*>& Integrantes){
+        string msg = "";
+        int x=0;
+
+        msg += "Aqui estao os dados sobre os integrantes!\n";
+
+        if (Integrantes.empty()) {
+            mensagem += "Nenhum integrante foi adicionado a essa banda ainda :(.";
+            } else {
+                for (const auto& Integrante : Integrantes){
+                    x++;
+                    msg = "Nome do integrante: " + to_string(Integrante->getNome) + " Idade: " + to_string(Integrante->getidade()) + " Instrumento: " + to_string(Integrante->getInstrumento) "\n.";
+                }
+                msg += ""
+        }
+    }
 }
