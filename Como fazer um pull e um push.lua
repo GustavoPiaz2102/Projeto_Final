0ce521d02826b3e01ed0e243eedbc133c09d125c
Gustavo <gustavopiazdasilva2102@gmail.com>
2024-10-31 15:50:20 -0300

    Teste

---
+++ Como fazer um pull e um push.lua
@@ -0,0 +1,12 @@
+Oque é pull?
+Pull é pra pegar os arquivos do git e trazer para o seu pc, é sempre bom fazer isso antes de alterar algo
+
+Oque é push? 
+Push é enviar os arquivos para o git
+
+como fazer no vs code?
+Para fazer o pull no vs code você vai no source control(embaixo da lupa) e se tiver um pull para fazer ele vai aparecer sync files
+
+Como fazer o push?
+No source control vai aparecer os arquivos que você alterou, então você aperta em commit & push(na seta ao lado do commit) e ele ira enviar os arquivos para o git
+(Sempre salve os arquivos antes de fazer o commit)
